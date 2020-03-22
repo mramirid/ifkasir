bc7120f09b27a09b903481c372b4cf4a3084899a
Amir <amir.hakim@kolu.web.id>
2020-03-22 12:45:44 +0700

    Update sidebar menu

--- application/views/layouts/_sidebar.php
+++ application/views/layouts/_sidebar.php
@@ -7,43 +7,92 @@
         <!-- Sidebar navigation-->
         <nav class="sidebar-nav">
             <ul id="sidebarnav">
-                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="<?= base_url('home') ?>"
-                        aria-expanded="false"><i data-feather="home" class="feather-icon"></i><span
-                            class="hide-menu">Dashboard</span></a></li>
+                <!-- Submemu Dashboard -->
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link" href="<?= base_url('home') ?>" aria-expanded="false">
+                        <i data-feather="home" class="feather-icon"></i>
+                        <span class="hide-menu">Dashboard</span>
+                    </a>
+                </li>
+
                 <li class="list-divider"></li>
+
+                <!-- Submemu Kasir -->
                 <li class="nav-small-cap"><span class="hide-menu">Kasir</span></li>
-
-                <li class="sidebar-item"> <a class="sidebar-link" href="ticket-list.html"
-                        aria-expanded="false"><i data-feather="shopping-cart" class="feather-icon"></i><span
-                            class="hide-menu">Pesan
-                        </span></a>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="<?= base_url('') ?>" aria-expanded="false">
+                        <i data-feather="shopping-cart" class="feather-icon"></i>
+                        <span class="hide-menu">Pesan</span>
+                    </a>
                 </li>
-                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-chat.html"
-                        aria-expanded="false"><i data-feather="list" class="feather-icon"></i><span
-                            class="hide-menu">List Penjualan</span></a></li>
+                <li class="sidebar-item"> 
+                    <a class="sidebar-link sidebar-link" href="<?= base_url('') ?>" aria-expanded="false">
+                        <i data-feather="list" class="feather-icon"></i>
+                        <span class="hide-menu">List Penjualan</span>
+                    </a>
+                </li>
 
                 <li class="list-divider"></li>
-                <li class="nav-small-cap"><span class="hide-menu">Manajemen Karyawan</span></li>
 
-                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="authentication-login1.html"
-                        aria-expanded="false"><i data-feather="users" class="feather-icon"></i><span
-                            class="hide-menu">List Karyawan
-                        </span></a>
+                <!-- Submemu Manajemen Inventory -->
+                <li class="nav-small-cap"><span class="hide-menu">Manajemen Inventory</span></li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link" href="<?= base_url('') ?>" aria-expanded="false">
+                        <i data-feather="box" class="feather-icon"></i>
+                        <span class="hide-menu">Inventory</span>
+                    </a>
                 </li>
-                <li class="sidebar-item"> <a class="sidebar-link sidebar-link"
-                        href="<?= base_url('register') ?>" aria-expanded="false"><i data-feather="user-plus"
-                            class="feather-icon"></i><span class="hide-menu">Register
-                        </span></a>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link" href="<?= base_url('') ?>" aria-expanded="false">
+                        <i data-feather="plus-square" class="feather-icon"></i>
+                        <span class="hide-menu">Tambah Stok</span>
+                    </a>
+                </li>
+
+                <li class="list-divider"></li>
+
+                <!-- Submemu Pembukuan -->
+                <li class="nav-small-cap"><span class="hide-menu">Pembukuan</span></li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link" href="<?= base_url('') ?>" aria-expanded="false">
+                        <i data-feather="book" class="feather-icon"></i>
+                        <span class="hide-menu">Pembukuan</span>
+                    </a>
+                </li>
+
+                <li class="list-divider"></li>
+
+                <!-- Submemu Manajemen Karyawan -->
+                <li class="nav-small-cap"><span class="hide-menu">Manajemen Karyawan</span></li>
+                <li class="sidebar-item"> 
+                    <a class="sidebar-link sidebar-link" href="<?= base_url('') ?>" aria-expanded="false">
+                        <i data-feather="users" class="feather-icon"></i>
+                        <span class="hide-menu">List Karyawan</span>
+                    </a>
+                </li>
+                <li class="sidebar-item"> 
+                    <a class="sidebar-link sidebar-link" href="<?= base_url('register') ?>" aria-expanded="false">
+                        <i data-feather="user-plus" class="feather-icon"></i>
+                        <span class="hide-menu">Register</span>
+                    </a>
                 </li>
                 
                 <li class="list-divider"></li>
+
+                <!-- Submemu Extra -->
                 <li class="nav-small-cap"><span class="hide-menu">Extra</span></li>
-                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="../../docs/docs.html"
-                        aria-expanded="false"><i data-feather="github" class="feather-icon"></i><span
-                            class="hide-menu">Repository</span></a></li>
-                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="authentication-login1.html"
-                        aria-expanded="false"><i data-feather="info" class="feather-icon"></i><span
-                            class="hide-menu">About</span></a></li>
+                <li class="sidebar-item"> 
+                    <a class="sidebar-link sidebar-link" href="<?= base_url('') ?>" aria-expanded="false">
+                        <i data-feather="github" class="feather-icon"></i>
+                        <span class="hide-menu">Repository</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link" href="<?= base_url('') ?>" aria-expanded="false">
+                        <i data-feather="info" class="feather-icon"></i>
+                        <span class="hide-menu">About</span>
+                    </a>
+                </li>
             </ul>
         </nav>
         <!-- End Sidebar navigation -->
