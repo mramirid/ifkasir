5c87be8836473c8d8afdaaa296189e3de280034e
Amir <amir.hakim@kolu.web.id>
2020-04-05 15:07:29 +0700

    Update about

--- application/views/pages/about/index.php
+++ application/views/pages/about/index.php
@@ -7,35 +7,99 @@
     
     <div class="row">
         <div class="col-md-12">
-            <div class="card-group">
-                <div class="card">
-                    <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/amir.jpg') ?>"
-                        alt="Card image cap">
-                    <div class="card-body">
-                        <h4 class="card-title">Amir Muhammad Hakim</h4>
-                        <p class="card-text">17081010051</p>
-                        <a href="https://github.com/mramirid" class="btn btn-primary">Github Account</a>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/alfath.png') ?>"
-                        alt="Card image cap">
-                    <div class="card-body">
-                        <h4 class="card-title">Alfath Daryl Alhajir</h4>
-                        <p class="card-text">17081010064</p>
-                        <a href="https://github.com/alfa-th" class="btn btn-primary">Github Account</a>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/aliev.png') ?>"
-                        alt="Card image cap">
-                    <div class="card-body">
-                        <h4 class="card-title">Aliev Wisnu Prasetyo</h4>
-                        <p class="card-text">17081010093</p>
-                        <a href="https://github.com/aliev99" class="btn btn-primary">Github Account</a>
+            <!-- Start Kelompok Inventory -->
+            <h3 class="page-title text-truncate text-dark font-weight-medium mb-4 text-center">Kelompok Inventory</h3>
+            <div class="row mb-5">
+                <div class="col-md-12">
+                    <div class="card-group">
+                        <div class="card">
+                            <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/default.jpg') ?>">
+                            <div class="card-body">
+                                <h4 class="card-title">Moh. Fathur Rohman</h4>
+                                <p class="card-text">17081010048</p>
+                                <a href="https://github.com/rahmanboy987" class="btn btn-primary">Github Account</a>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/default.jpg') ?>">
+                            <div class="card-body">
+                                <h4 class="card-title">Guntur Adhi Prasetya</h4>
+                                <p class="card-text">17081010043</p>
+                                <a href="https://github.com/GugunAP" class="btn btn-primary">Github Account</a>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/default.jpg') ?>">
+                            <div class="card-body">
+                                <h4 class="card-title">Guruh Ihda Alfi Ath Thaariq</h4>
+                                <p class="card-text">17081010038</p>
+                                <a href="#" class="btn btn-primary">Github Account</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+            <!-- End Kelompok Inventory -->
+            <!-- Start Kelompok Kasir -->
+            <h3 class="page-title text-truncate text-dark font-weight-medium mb-4 text-center">Kelompok Kasir</h3>
+            <div class="row mb-5">
+                <div class="col-md-12">
+                    <div class="card-group">
+                        <div class="card">
+                            <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/amir.jpg') ?>">
+                            <div class="card-body">
+                                <h4 class="card-title">Amir Muhammad Hakim</h4>
+                                <p class="card-text">17081010051</p>
+                                <a href="https://github.com/mramirid" class="btn btn-primary">Github Account</a>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/alfath.png') ?>">
+                            <div class="card-body">
+                                <h4 class="card-title">Alfath Daryl Alhajir</h4>
+                                <p class="card-text">17081010064</p>
+                                <a href="https://github.com/alfa-th" class="btn btn-primary">Github Account</a>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/aliev.png') ?>">
+                            <div class="card-body">
+                                <h4 class="card-title">Aliev Wisnu Prasetyo</h4>
+                                <p class="card-text">17081010093</p>
+                                <a href="https://github.com/aliev99" class="btn btn-primary">Github Account</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Kelompok Kasir -->
+            <!-- Start Kelompok Pembukuan -->
+            <h3 class="page-title text-truncate text-dark font-weight-medium mb-4 text-center">Kelompok Pembukuan</h3>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card-group">
+                        <div class="card">
+                            <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/default.jpg') ?>">
+                            <div class="card-body">
+                                <h4 class="card-title">Fahmi Fachrurroziq</h4>
+                                <p class="card-text">17081010079</p>
+                                <a href="#" class="btn btn-primary">Github Account</a>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/default.jpg') ?>">
+                            <div class="card-body">
+                                <h4 class="card-title">Mifa Abiyyu Hibatulloh</h4>
+                                <p class="card-text">17081010090</p>
+                                <a href="#" class="btn btn-primary">Github Account</a>
+                            </div>
+                        </div>
+                        <div class="card">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Kelompok Pembukuan -->
         </div>
     </div>
 </div>
